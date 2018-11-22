303d153d942ff4c15ff7aabd5796e6b3aa189a0c
Ansh Gujral <anshgujral1997@gmail.com>
2018-11-22 21:39:45 +0530

    created doctor and patient app
    Signed-off-by: Ansh Gujral <anshgujral1997@gmail.com>

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,7 @@
 from Crypto.Cipher import AES
-obj = AES.new('This is a key12w', AES.MODE_CFB, 'This is an IV456')
-message = "The answer is no"
+obj = AES.new('This is a key12w', AES.MODE_CFB, '1234561234561234')
+message = ""
 ciphertext = obj.encrypt(message)
 print (ciphertext)
-obj2 = AES.new('This is a key12w', AES.MODE_CFB, 'This is an IV456')
+obj2 = AES.new('This is a key12w', AES.MODE_CFB, '1234561234561234')
 print (obj2.decrypt(ciphertext).decode('utf-8'))
